23b7c15b53c2910f94b3116b800906393b97d348
Jorge Andrés Mena <jormencar@yahoo.com>
2021-06-13 02:42:46 -0400

    fist commit

---
+++ src/Photo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Photo = ({
+  urls: { regular },
+  alt_description,
+  likes,
+  user: {
+    name,
+    portfolio_url,
+    profile_image: { medium },
+  },
+}) => {
+  return (
+    <article className="photo">
+      <img
+        loading="lazy"
+        src={regular}
+        alt={alt_description ? alt_description : 'image from unplash'}
+      />
+
+      <div className="photo-info">
+        <div>
+          <h4>{name}</h4>
+          <p>{likes} likes</p>
+        </div>
+        <a href={portfolio_url}>
+          <img src={medium} alt={name} className="user-img" />
+        </a>
+      </div>
+    </article>
+  );
+};
+
+export default Photo;
